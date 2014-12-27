11454fe63b51327ab3cbc838ec1c3cfd5a2d7a63
QuarK264 <alexey.danilov@hotmail.com>
2014-12-27 03:06:19 +0300

    Сдеалал 7.1.

--- DAA.TP.lab7/DAA.TP.lab7/Program.cs
+++ DAA.TP.lab7/DAA.TP.lab7/Program.cs
@@ -13,11 +13,39 @@
             Pizzeria Pizza = new Pizzeria();
             Pizza.AddOrder("Крюков", "Архангельск", "345678");
             Pizza.AddOrder("Ильченко", "Северодвинск", "654587");
-            Pizza.RemoveOrder("Ильченко");
-            Pizza.RemoveOrder("Юшманов");
-            Pizza.ChangeOrderInfo("Крюков", "Крюков", "Ломоносовский район", "236578");
-            Console.ReadKey(true);
+            Pizza.AddOrder("Данилов", "Архангельск", "158356");
+            Pizza.AddOrder("Игорь", "Архангельск", "278905");
+            
+            Console.WriteLine("Сделаны заказы:");
+            foreach (Order order  in Pizza.ListofOrders)
+            {
+                Console.WriteLine(order.ToString());
+            }
 
+            Console.WriteLine("Заказы отменены:");
+            Pizza.RemoveOrder("Ильченко", "Северодвинск", "654587");
+            Pizza.RemoveOrder("Юшманов", "Архангельск", "426414");            
+            foreach (Order order in Pizza.ListofOrders)
+            {
+                Console.WriteLine(order.ToString());
+            }
+
+            Pizza.ChangeOrderInfo("Крюков", "Крюков", "Ломоносовский район", "236578");
+            Console.WriteLine("Заказы изменены:");
+            foreach (Order order in Pizza.ListofOrders)
+            {
+                Console.WriteLine(order.ToString());
+            }
+            Console.WriteLine("Заказы из с одного адреса:");
+            Pizza.PrintOrdersFromAddress("Архангельск");
+            Pizza.AddOrder("Крюков", "10 корпус", "453254");
+            Pizza.AddOrder("Крюков", "Привокзальный", "564532");
+            Pizza.AddOrder("Кирьянов", "Соломбала", "342456");
+
+
+            Pizza.PrintPersonsWithMostOrdersInfo();
+            Pizza.PrintPersonsWithLessOrdersInfo();
+            Console.ReadKey(true);
         }
     }
 }
